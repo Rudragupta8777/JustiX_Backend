bef23d3e5f76a24ee294e5e4033e7137e8a9c625
Rudragupta8777 <rudragupta1204@gmail.com>
2026-02-12 04:50:01 +0530

    Ready for deploy

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,14 @@
-const admin = require('firebase-admin');
-const path = require('path');
+const admin = require("firebase-admin");
 
-const serviceAccount = require(path.join(__dirname, '../serviceAccountKey.json'));
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // If on Render, parse the string back into an object
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // If local, require the file (make sure this file is in .gitignore!)
+  serviceAccount = require("../serviceAccount.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
